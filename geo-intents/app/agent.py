851075e0050ad2a4fcb2f854523c3a58749d9e4d
reubenrows-ui <reubenrows@gmail.com>
2025-09-11 07:13:28 -0400

    added second validator

--- geo-intents/app/agent.py
+++ geo-intents/app/agent.py
@@ -10,10 +10,13 @@ root_agent = LlmAgent(
     name="intent_router",
     model="gemini-2.5-flash",
     instruction="""You are a router agent that directs user queries to the appropriate sub-agent based on intent.
-    - If you detect the user is asking about nearby places, locations, or competitors, transfer to the nearby_intent agent.
+    - If you get any questions related to finding nearby places, restaurants, or points of interest, transfer to the nearby_intent agent. See examples below:
+        - “What cafés are within 1 mile of Times Square?”
+        - “How many competitors are near my downtown Vancouver store?”
+        - “List restaurants close to Union Station, Toronto.”
     - If the user is asking about anything else, transfer to the unknown_intent agent.
     """,
-    description="internt router.",
+    description="internt_router.",
     # allow_transfer=True is often implicit with sub_agents in AutoFlow
     sub_agents=[
         nearby_intent_agent, 
--- geo-intents/app/agents/nearby/agent.py
+++ geo-intents/app/agents/nearby/agent.py
@@ -4,6 +4,8 @@ from google.adk.tools import VertexAiSearchTool
 
 from .schema import SlotCandidates, PoiCoords
 
+DATASTORE_PATH = "projects/kaggle-hackathon-471317/locations/global/collections/default_collection/dataStores/poi-locations-data"
+
 entity_extractor_agent = LlmAgent(
     name="EntityExtractor", 
     model="gemini-2.5-flash",
@@ -14,6 +16,13 @@ entity_extractor_agent = LlmAgent(
     - radius_value: number if user specifies a distance (e.g., 2)
     - radius_unit: unit of distance (km, miles, mi, m)
     - entity: competitor or brand name (e.g., 'Starbucks')
+    Here are some examples of user requests:
+    - What cafés are within 1 mile of Times Square?
+        - Payload: anchor_place: Times Square, radius_value: 1, radius_unit: mile, entity: café
+    - How many competitors are near my downtown Vancouver store?”
+        - Payload: anchor_place: downtown Vancouver store, radius_value: null, radius_unit: null, entity: competitors
+    - List cafe's  close to Union Station, Toronto.”
+        - Payload: anchor_place: Union Station, Toronto, radius_value: null, radius_unit: null, entity: cafe's
     Respond ONLY with a JSON object matching this exact schema:
     {json.dumps(SlotCandidates.model_json_schema(), indent=2)}
     Use your knowledge to determine the candidate terms or snippets.
@@ -21,7 +30,6 @@ entity_extractor_agent = LlmAgent(
     output_key="slot_candidates",
     )
 
-DATASTORE_PATH = "projects/kaggle-hackathon-471317/locations/global/collections/default_collection/dataStores/poi-locations-data"
 
 anchor_place_validator_agent = LlmAgent(
     name="AnchorPlaceValidator",
@@ -39,6 +47,22 @@ anchor_place_validator_agent = LlmAgent(
 
 ) # Custom non-LLM agent
 
+entity_validator_agent = LlmAgent(
+    name="AnchorPlaceValidator",
+    input_schema=SlotCandidates,
+    instruction=f"""You are an agent that validates if the extracted entity matches any known places in the datastore.
+    Use the provided tool to search the datastore for places matching the extracted entity.
+        If you are confident there is a match, return the latitude and longitude of the best matching place.
+        If no match is found, return document_name as an empty response for latitude and longitude.
+    Respond ONLY with a JSON object matching this exact schema:
+    {json.dumps(PoiCoords.model_json_schema(), indent=2)}
+
+    """,
+    tools = [VertexAiSearchTool(data_store_id=DATASTORE_PATH,max_results=1)],
+    output_key="entity_coords",
+
+) # Custom non-LLM agent
+
 nearby_intent_agent= SequentialAgent(
     name="nearby_intent_agent",
-    sub_agents=[entity_extractor_agent, anchor_place_validator_agent])
+    sub_agents=[entity_extractor_agent, anchor_place_validator_agent, entity_validator_agent])
